52b3cb40600466fa38f27ff621b5328a9d6dc659
lihaoran <144754858+LEEGOAL418@users.noreply.github.com>
2026-03-26 14:31:16 +0800

    Update MACE_DESSP.sh

--- mace-train/MACE_DESSP.sh
+++ mace-train/MACE_DESSP.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
-CUDA_VISIBLE_DEVICES=0,1 mace_run_train \
+CUDA_VISIBLE_DEVICES=0,1 torchrun --standalone --nnodes=1 --nproc_per_node=2 \
+    mace_run_train \
     --config="./configs/MACE_DESSP.yaml" \
     --distributed
