cecb1012071396a82843dfaf975e59057340e828
lihaoran <144754858+LEEGOAL418@users.noreply.github.com>
2026-03-26 14:29:44 +0800

    Update MACE_ORB_Distilled.sh

--- mace-train/MACE_ORB_Distilled.sh
+++ mace-train/MACE_ORB_Distilled.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-# CUDA_VISIBLE_DEVICES=0 python /home/user/Desktop/LHR/MACE-ASE/mace/cli/run_train.py \
-#     --config="./configs/MACE-V3_vasponly_ft32.yaml"
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --standalone --nnodes=1 --nproc_per_node=4 \
     mace_run_train \
-    --distributed \
-    --config "./configs/MACE_ORB_Distilled.yaml"
+    --config "./configs/MACE_ORB_Distilled.yaml" \
+    --distributed
+    
