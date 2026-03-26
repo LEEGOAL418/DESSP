91e260f01dac5df8796bd0a0f14e5f744f38821b
lihaoran <144754858+LEEGOAL418@users.noreply.github.com>
2026-03-26 14:30:11 +0800

    Update MACE_VASPGO.sh

--- mace-train/MACE_VASPGO.sh
+++ mace-train/MACE_VASPGO.sh
@@ -2,6 +2,7 @@
 # export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 CUDA_VISIBLE_DEVICES=2,3 torchrun --standalone --nnodes=1 --nproc_per_node=2 \
-    ../mace/cli/run_train.py \
-    --distributed \
-    --config "./configs/MACE_VASPGO.yaml"
+    mace_run_train \
+    --config "./configs/MACE_VASPGO.yaml" \
+    --distributed
+    
